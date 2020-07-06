7f6036d1157fc1a2601f6fb9a9321db41414bbf1
Sun <ksun@nrel.gov>
2020-07-06 10:27:39 -0800

    Updated File Locations

---
+++ examples/coupling_examples/Layout_1x2.py
@@ -0,0 +1,91 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from floris.induction import options_dict
+from helper_functions import *
+
+fontsize = 12
+plt.rc('font', family='serif')
+plt.rc('font', size=12)
+
+# --- Plot options
+bExport=False
+bCompute=True
+bColorBar=False
+nStreamlines=0
+U0 =6
+minSpeed=0.5
+maxSpeed=1.03
+levelsLines=np.sort([1.05,1.0,0.99,0.98,0.95,0.9,0.5])
+
+# ---
+ny=30 # 200
+nx=ny*4
+
+input_file="Layout_1x2.json"
+input_dict=None
+D=126
+resolution=Vec3(nx, ny, 2)
+bounds=[-4*D,14*D,-2*D-10,2*D+10,89,90] # xmin xmax .. zmin zmax
+Ind_Opts=options_dict()
+Ind_Opts['Rfact']=1.0
+Ind_Opts['GammaFact']=1.0
+Ind_Opts['Ground']=True
+
+
+titles=[]
+titles.append('FLORIS (original)')
+titles.append('FLORIS (with induction)')
+# titles.append('With induction and blending')
+
+# --- Parametric computation
+if bCompute:
+    planes=[]
+    print('-----------------------------------------------------------------')
+    Ind_Opts['no_induction']=True
+    planes.append(get_HH_plane_vel(input_file, input_dict, Ind_Opts, resolution, bounds_to_set=bounds))
+    savePlane(planes[-1],'_data/Layout12_0_',U0=U0)
+
+    print('-----------------------------------------------------------------')
+    Ind_Opts['no_induction']=False
+    Ind_Opts['blend']=False
+    planes.append(get_HH_plane_vel(input_file, input_dict, Ind_Opts, resolution, bounds_to_set=bounds))
+    savePlane(planes[-1],'_data/Layout12_1_',U0=U0)
+
+planes=[]
+planes.append(loadPlane('_data/Layout12_0_'))
+planes.append(loadPlane('_data/Layout12_1_'))
+
+
+# --- Plot and show
+fig, axes = plt.subplots(nrows=len(titles), ncols=1, sharex=True, sharey=True, figsize=(12.0, 6.0))
+for i,(ax,p,t) in enumerate(zip(axes.flat,planes,titles)):
+    x=p[0]
+    y=p[1]
+    u=p[2]
+    v=p[3]
+    im = plotPlane(x/D,y/D,u,v,ax,minSpeed=minSpeed,maxSpeed=maxSpeed,
+            nStreamlines=nStreamlines,levelsLines=levelsLines, axial=True, colors='k')
+    ax.title.set_text(t)
+
+    ax.set_ylabel('r/D [-]')
+    ax.title.set_text(t)
+    ax.tick_params(direction='in')
+
+    if i==1:
+        ax.set_xlabel('z/D [-]')
+
+ax.set_xlim([-4,14])
+ax.set_ylim([-2,2])
+
+if bColorBar:
+    fig.subplots_adjust(left=0.08, right=0.83, top=0.93, bottom=0.11,hspace=0.17)
+    cbar_ax = fig.add_axes([0.88, 0.11, 0.04, 0.82])
+    cbar=fig.colorbar(im, cax=cbar_ax)
+    cbar.set_ticks(levelsLines)
+    cbar.set_ticklabels([str(v) if v not in [0.99] else '' for v in levelsLines])
+    cbar.ax.tick_params(axis='both', direction='in',length=18,color=(0.5,0.5,0.5))
+else:
+    fig.subplots_adjust(left=0.035, right=0.990, top=0.96, bottom=0.08,hspace=0.17)
+
+plt.show()
---
+++ examples/coupling_examples/helper_functions.py
@@ -0,0 +1,90 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+import matplotlib.colors as mcolors
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+# --- Local
+import floris.tools as wfct
+from floris.utilities import Vec3
+
+def get_HH_plane_vel(input_file, input_dict, Ind_Opts, resolution=Vec3(232, 114, 3),bounds_to_set=None):
+    fi = wfct.floris_interface.FlorisInterface(input_file, input_dict)
+    # Calculate wake
+    #fi.calculate_wake(Ind_Opts=Ind_Opts)
+
+    #fd=fi.get_flow_data(resolution = resolution, Ind_Opts=Ind_Opts)
+
+    # # Initialize the horizontal cut
+    hor_plane = fi.get_hor_plane(
+        height = fi.floris.farm.turbines[0].hub_height,
+        x_resolution = resolution.x1,
+        y_resolution = resolution.x2,
+        x_bounds = tuple(bounds_to_set[0:2]),
+        y_bounds = tuple(bounds_to_set[2:4]),
+        Ind_Opts = Ind_Opts
+        )
+    u_mesh = hor_plane.df.u.values.reshape(resolution.x2,resolution.x1)
+    v_mesh = hor_plane.df.v.values.reshape(resolution.x2,resolution.x1)
+    
+    return np.unique(hor_plane.df.x1),np.unique(hor_plane.df.x2), u_mesh, v_mesh, fi
+
+
+def savePlane(x,y=None,u=None,v=None,base='',U0=None):
+    if u is None:
+        p=x
+        base=y
+        x=p[0]
+        y=p[1]
+        u=p[2]
+        v=p[3]
+    if U0 is not None:
+        u=u/U0
+        v=v/U0
+
+    base = os.path.normpath(base)
+    np.save(base+'x.npy',x)
+    np.save(base+'y.npy',y)
+    np.save(base+'u.npy',u)
+    np.save(base+'v.npy',v)
+
+
+def loadPlane(base=''):
+    base = os.path.normpath(base)
+    x = np.load(base+'x.npy')
+    y = np.load(base+'y.npy')
+    u = np.load(base+'u.npy')
+    v = np.load(base+'v.npy')
+    return x,y,u,v
+
+
+def plotPlane(x,y,u,v,ax,minSpeed=None,maxSpeed=None, cmap='coolwarm', colors='w', linewidths=0.8, alpha=0.3,nStreamlines=0,levelsContour=None,levelsLines=None,axial=False):
+    if axial:
+        Speed=u
+    else:
+        Speed=np.sqrt(u**2+v**2)
+    if minSpeed is None:
+        minSpeed = Speed.min()
+        maxSpeed = Speed.max()
+
+    if not ax:
+        fig, ax = plt.subplots()
+    Z = np.ma.masked_where(np.isnan(Speed), Speed)
+
+    # Plot the cut-through
+    im = ax.pcolormesh(x, y, Z, cmap=cmap, vmin=minSpeed, vmax=maxSpeed)
+    if levelsLines is not None:
+        rcParams['contour.negative_linestyle'] = 'solid'
+        cs=ax.contour(x, y, Z, levels=levelsLines, colors=colors, linewidths=linewidths, alpha=alpha)
+
+    if nStreamlines>0:
+        yseed=np.linspace(min(y)*0.9,max(y)*0.9,nStreamlines)
+        start=np.array([yseed*0,yseed])
+        sp=ax.streamplot(x,y,u,v,color='k',start_points=start.T,linewidth=0.7,density=30,arrowstyle='-')
+#         qv=streamQuiver(ax,sp,n=5,scale=40,angles='xy')
+
+    ax.set_aspect('equal')
+
+    return im
