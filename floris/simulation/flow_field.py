Fix GCH calculation with Induction Iteration

floris/simulation/flow_field.py
@@ -21,9 +21,7 @@ import copy
 from ..utilities import Vec3, cosd, sind, tand
 
 #import wiz
-#from floris.simulation.wake_vortex.Solver import Ct_const_cutoff
 from floris.simulation.wake_vortex.Solver import Ct_const_cutoff
-# from floris.induction import options_dict
 
 import time
 def pretty_time(t):
@@ -721,6 +719,9 @@ class FlowField:
                     rotated_x = initial_rotated_x - x_grid_offset
 
         def compute_wakes(u_wake,v_wake,w_wake,u_ind=None,v_ind=None,w_ind=None):
+            self.v = np.zeros(np.shape(self.u))
+            self.w = np.zeros(np.shape(self.u))
+            
             if u_ind is None:
                 u_ind = 0
 
@@ -837,6 +838,7 @@ class FlowField:
                     if self.wake.velocity_model.model_string == "curl":
                         self.v = turb_v_wake
                         self.w = turb_w_wake
+                    # Why is this section different
                     else:
                         self.v = self.v + turb_v_wake
                         self.w = self.w + turb_w_wake
@@ -897,7 +899,7 @@ class FlowField:
         PowerTot=0
         nWT=len(sorted_map)
         for i, (coord,turbine) in enumerate(sorted_map):
-            # print('turbine %d:' %i,turbine.power)
+            print('turbine %d:' %i,turbine.power)
             PowerTot+=turbine.power
         print('Avg Power per WT:',PowerTot/nWT/1000)
 
@@ -929,11 +931,11 @@ class FlowField:
                 #print_Ct()
             PowerTot=0
             nWT=len(sorted_map)
-            for coord,turbine in sorted_map:
-                # print('turbine %d:' %i,turbine.power)
+            for i, (coord,turbine) in enumerate(sorted_map):
+                print('turbine %d:' %i,turbine.power)
                 PowerTot+=turbine.power
             print('Avg Power per WT:',PowerTot/nWT/1000)
-
+            self.u_induct = u_ind
             u_wake = (u_wake - u_ind)
             v_wake = (v_wake + v_ind)
             w_wake = (w_wake + w_ind)
@@ -965,11 +967,12 @@ class FlowField:
                         #print_Ct()
                     PowerTot=0
                     nWT=len(sorted_map)
-                    for coord,turbine in sorted_map:
-                        # print('turbine %d:' %i,turbine.power)
+                    for i, (coord,turbine) in enumerate(sorted_map):
+                        print('turbine %d:' %i,turbine.power)
                         PowerTot+=turbine.power
                     print('Avg Power per WT:',PowerTot/nWT/1000)
 
+                self.u_induct = u_ind
                 u_wake = (u_wake - u_ind)
                 v_wake = (v_wake + v_ind)
                 w_wake = (w_wake + w_ind)

floris/tools/floris_interface.py
@@ -94,7 +94,8 @@ class FlorisInterface(LoggerBase):
 
         if Ind_Opts is None:
             Ind_Opts = self.IndOpts
-        # print('Calculate Wake IndOpts: ',Ind_Opts)
+        # else: self.IndOpts = Ind_Opts
+        # print("Ind_Opts calculate wake: ", Ind_Opts)
 
         self.floris.farm.flow_field.calculate_wake(
             no_wake=no_wake,
@@ -347,6 +348,11 @@ class FlorisInterface(LoggerBase):
         if normal_vector == "x":
             points = np.row_stack((x3_array, x1_array, x2_array))
 
+        if Ind_Opts is None:
+            Ind_Opts = self.IndOpts
+        # else: self.IndOpts = Ind_Opts
+        # print("Ind_Opts get_plane_of_points: ", Ind_Opts)
+
         # Recalculate wake with these points
         flow_field.calculate_wake(points=points,Ind_Opts=Ind_Opts)
 
@@ -357,6 +363,9 @@ class FlorisInterface(LoggerBase):
         u_flat = flow_field.u.flatten()
         v_flat = flow_field.v.flatten()
         w_flat = flow_field.w.flatten()
+        if Ind_Opts['induction']:
+            u_ind_flat = flow_field.u_induct.flatten()
+        else: u_ind_flat = np.zeros(np.shape(u_flat))
 
         # Create a df of these
         if normal_vector == "z":
@@ -368,6 +377,7 @@ class FlorisInterface(LoggerBase):
                     "u": u_flat,
                     "v": v_flat,
                     "w": w_flat,
+                    "u_ind": u_ind_flat
                 }
             )
         if normal_vector == "x":
@@ -379,6 +389,7 @@ class FlorisInterface(LoggerBase):
                     "u": u_flat,
                     "v": v_flat,
                     "w": w_flat,
+                    "u_ind": u_ind_flat
                 }
             )
         if normal_vector == "y":
@@ -390,6 +401,7 @@ class FlorisInterface(LoggerBase):
                     "u": u_flat,
                     "v": v_flat,
                     "w": w_flat,
+                    "u_ind": u_ind_flat
                 }
             )
 
@@ -512,7 +524,8 @@ class FlorisInterface(LoggerBase):
         
         if Ind_Opts is None:
             Ind_Opts = self.IndOpts
-        # print('Get_hor_plane IndOpts: ',Ind_Opts)
+        # else: self.IndOpts = Ind_Opts
+        # print("Ind_Opts get_hor_plane: ", Ind_Opts)
 
         # Get the points of data in a dataframe
         df = self.get_plane_of_points(
@@ -671,6 +684,12 @@ class FlorisInterface(LoggerBase):
         flow_field.reinitialize_flow_field(with_resolution=resolution, bounds_to_set=bounds_to_set)
         self.logger.info(resolution)
         # print(resolution)
+
+        if Ind_Opts is None:
+            Ind_Opts = self.IndOpts
+        # else: self.IndOpts = Ind_Opts
+        # print("Ind_Opts get_flow_data: ", Ind_Opts)
+
         flow_field.calculate_wake(Ind_Opts=Ind_Opts)
 
         order = "f"